67441796c0c6da6e3d1964808832fd60c9f6b284
Tharindu Senapathi <snptha002@myuct.ac.za>
2018-10-04 17:08:34 +0200

    PEP8 updates

--- tools/MDAnalysis/distance.py
+++ tools/MDAnalysis/distance.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-import sys, os
-import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import argparse
-import MDAnalysis
-import numpy.linalg
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+import MDAnalysis as mda
+import numpy as np
+
+matplotlib.use('Agg')
+
 
 def parse_command_line(argv):
     parser = argparse.ArgumentParser()
@@ -23,30 +25,31 @@ def parse_command_line(argv):
     parser.add_argument('--odistance_plot', help='odistance plot')
     return parser.parse_args()
 
+
 args = parse_command_line(sys.argv)
 
-atom1 = "(segid %s and resid %s and name %s)" % (args.isegid1, args.iresid1, args.iname1)
-atom2 = "(segid %s and resid %s and name %s)" % (args.isegid2, args.iresid2, args.iname2)
+atom1 = "(segid %s and resid %s and name %s)" % \
+    (args.isegid1, args.iresid1, args.iname1)
+atom2 = "(segid %s and resid %s and name %s)" % \
+    (args.isegid2, args.iresid2, args.iname2)
 
-u = MDAnalysis.Universe(args.ipdb, args.idcd, topology_format="PDB", format="DCD")
+u = mda.Universe(args.ipdb, args.idcd, topology_format="PDB", format="DCD")
 x = u.select_atoms(atom1)
 y = u.select_atoms(atom2)
 
 with open(args.output, 'w') as f:
-   for t in u.trajectory:
-       r = x.positions - y.positions
-       d = numpy.linalg.norm(r)
-       f.write(str(t.frame)+'\t ')
-       f.write(str(d)+'\n')
+    for t in u.trajectory:
+        r = x.positions - y.positions
+        d = np.linalg.norm(r)
+        f.write(str(t.frame) + '\t ')
+        f.write(str(d) + '\n')
 
 with open(args.output) as f:
-     f=[x.strip() for x in f if x.strip()]
-     data=[tuple(map(float,x.split())) for x in f[0:]]
-     time=[x[0] for x in data]
-     distance=[x[1] for x in data]
-     plt.plot(time, distance)
-     plt.xlabel('Frame No.')
-     plt.ylabel('Distance ($\AA$)')
-     plt.savefig(args.odistance_plot, format='png')
-
-
+    g = [xtmp.strip() for xtmp in f]
+    data = [tuple(map(float, xtmp.split())) for xtmp in g[0:]]
+    time = [xtmp[0] for xtmp in data]
+    distance = [xtmp[1] for xtmp in data]
+    plt.plot(time, distance)
+    plt.xlabel('Frame No.')
+    plt.ylabel('Distance ($\AA$)')
+    plt.savefig(args.odistance_plot, format='png')
