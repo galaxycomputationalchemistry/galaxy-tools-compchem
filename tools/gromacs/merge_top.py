a3de9b940e46999b5a09c63c59dd6a129e8b64cb
simonbray <sbray1371@gmail.com>
2019-03-27 21:39:35 +0100

    lint

--- tools/gromacs/merge_top.py
+++ tools/gromacs/merge_top.py
@@ -1,17 +1,22 @@
 import sys
 import re
 
+
 def combine_tops(top_text, itp_texts):
     """
-    Search through parent topology top_text and replace #include lines with the relevant child topologies from the dictionary itp_texts
+    Search through parent topology top_text and replace
+    #include lines with the relevant child topologies
+    from the dictionary itp_texts
     """
     for itp in itp_texts:
-        spl = re.split('#include ".*{}"\n'.format(itp), top_text) # split on include string
+        # split on include string, then rejoin around itp file
+        spl = re.split('#include ".*{}"\n'.format(itp), top_text)
         top_text = itp_texts[itp].join(spl)
     return top_text
 
-top = sys.argv[1] # parent topology file
-itps_file = sys.argv[2] # file with list of child topologies (.itp files)
+
+top = sys.argv[1]  # parent topology file
+itps_file = sys.argv[2]  # file with list of child topologies (.itp files)
 
 with open(itps_file) as f:
     itps = f.read().split()
@@ -19,7 +24,7 @@ with open(itps_file) as f:
 with open(top, 'r') as f:
     top_text = f.read()
 
-itp_texts = {} # create dictionary of child topologies
+itp_texts = {}  # create dictionary of child topologies
 for itp in itps:
     with open(itp, 'r') as f:
         itp_texts[itp] = f.read()
@@ -29,4 +34,5 @@ for itp in itp_texts:
     itp_texts[itp] = combine_tops(itp_texts[itp], itp_texts)
 
 with open('top_output.top', 'w') as f:
-    f.write(combine_tops(top_text, itp_texts)) # now combine all children into the parent
+    # now combine all children into the parent
+    f.write(combine_tops(top_text, itp_texts))
