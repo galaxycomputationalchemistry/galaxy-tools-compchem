8e8a0149d97f0f0e5888eac0443d4d97e4246f81
thepineapplepirate <braubeno@uno.edu>
2024-06-05 15:12:38 -0400

    ran flake8 and fixed the python script, removed structural biology from .shed.yml

--- tools/buildtools/protein_structure_rebuilders/BioPDB/BioPDB_align_and_rmsd.py
+++ tools/buildtools/protein_structure_rebuilders/BioPDB/BioPDB_align_and_rmsd.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 # The MIT License
-# 
+#
 # Copyright (c) 2010-2016 Anders S. Christensen
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,10 +22,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
-
 import Bio.PDB
 import argparse
+
 
 def __main__():
     parser = argparse.ArgumentParser(
@@ -53,11 +52,11 @@ def __main__():
     # Select what residues numbers you wish to align
     # and put them in a list
     start_id = int(args.start_residue)
-    end_id   = int(args.end_residue)
+    end_id = int(args.end_residue)
     atoms_to_be_aligned = range(start_id, end_id + 1)
 
     # Start the parser
-    pdb_parser = Bio.PDB.PDBParser(QUIET = True)
+    pdb_parser = Bio.PDB.PDBParser(QUIET=True)
 
     # Get the structures
     ref_structure = pdb_parser.get_structure("reference", args.ref_structure)
@@ -65,7 +64,7 @@ def __main__():
 
     # Use the first model in the pdb-files for alignment
     # Change the number 0 if you want to align to another structure
-    ref_model    = ref_structure[0]
+    ref_model = ref_structure[0]
     sample_model = sample_structure[0]
 
     # Make a list of the atoms (in the structures) you wish to align.
@@ -75,18 +74,18 @@ def __main__():
 
     # Iterate of all chains in the model in order to find all residues
     for ref_chain in ref_model:
-      # Iterate of all residues in each model in order to find proper atoms
-      for ref_res in ref_chain:
-        # Check if residue number ( .get_id() ) is in the list
-        if ref_res.get_id()[1] in atoms_to_be_aligned:
-          # Append CA atom to list
-          ref_atoms.append(ref_res['CA'])
+        # Iterate of all residues in each model in order to find proper atoms
+        for ref_res in ref_chain:
+            # Check if residue number ( .get_id() ) is in the list
+            if ref_res.get_id()[1] in atoms_to_be_aligned:
+                # Append CA atom to list
+                ref_atoms.append(ref_res['CA'])
 
     # Do the same for the sample structure
     for sample_chain in sample_model:
-      for sample_res in sample_chain:
-        if sample_res.get_id()[1] in atoms_to_be_aligned:
-          sample_atoms.append(sample_res['CA'])
+        for sample_res in sample_chain:
+            if sample_res.get_id()[1] in atoms_to_be_aligned:
+                sample_atoms.append(sample_res['CA'])
 
     # Now we initiate the superimposer:
     super_imposer = Bio.PDB.Superimposer()
@@ -94,14 +93,14 @@ def __main__():
     super_imposer.apply(sample_model.get_atoms())
 
     # Save RMSD into an output file:
-    with open (args.rmsd, 'w') as rmsd_out:
-      rmsd_out.write(str(super_imposer.rms))
+    with open(args.rmsd, 'w') as rmsd_out:
+        rmsd_out.write(str(super_imposer.rms))
 
     # Save aligned coordinates of the model:
     io = Bio.PDB.PDBIO()
-    io.set_structure(sample_structure) 
+    io.set_structure(sample_structure)
     io.save(args.aligned_structure)
+
 
 if __name__ == "__main__":
     __main__()
-
